0d2d9b06647b193884ed50b28d6bb8869749855b
unknown <angeleenabesamis@outlook.com>
2021-02-18 23:11:55 -0600

    xUnit Exercise

---
+++ Calculator - csharp-2/Calculator/Calculator/BasicMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class BasicMath
+    {
+        public int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public int Subtract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+
+        public int Multiply(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        public int Division(int n1, int n2)
+        {
+            if (n2 == 0)
+                return 0;
+            return n1 / n2;
+        }
+    }
+}
---
+++ Calculator - csharp-2/Calculator/TestMyCal/TestBasicMath.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Calculator;
+using Xunit;
+
+namespace TestMyCal
+{
+    public class TestBasicMath
+    {
+        [Fact]
+        public void AddNumbers()
+        {
+            //arrange
+            int expected = 5;
+
+            //action
+            BasicMath calc1 = new BasicMath();
+            int actual = calc1.Add(2, 3);
+
+            //assertion
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubtractNumbers()
+        {
+            //arrange
+            int expected = -10;
+
+            //action
+            BasicMath calc2 = new BasicMath();
+            int actual = calc2.Subtract(-20, -10);
+
+            //assertion
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MultiplyNumbers()
+        {
+            //arrange
+            int expected = 100;
+
+            //action
+            BasicMath calc3 = new BasicMath();
+            int actual = calc3.Multiply(50, 2);
+
+            //assertion
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(4, 0, 0)]
+        [InlineData(0, 4, 0)]
+        [InlineData(10, 5, 2)]
+        public void DivideNumbers(int a, int b, int expected)
+        {
+            //action
+            BasicMath calc3 = new BasicMath();
+            int actual = calc3.Division(a, b);
+
+            //assertion
+            Assert.Equal(expected, actual);
+            //Assert.eq
+        }
+    }
+}
